4ca1b69ef46047390c62b86dc8f97b6482b6f8b8
Ravi Shankar Mehta <nitravi.m@gmail.com>
2022-11-26 23:22:26 +0530

    ingore nodemodule

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const product = require('./routes/productRoutes');
 const createProduct= require('./routes/productRoutes');
 const user= require('./routes/userRoutes');
 
+
 app.use('/api/v1',product);
 app.use('/api/v1',createProduct);
 app.use('/api/v1',user);
